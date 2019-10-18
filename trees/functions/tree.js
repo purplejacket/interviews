b9ac58dbcf4189f6f9e62a9e9e24a4434f81013e
Marc Cull <mcull@users.noreply.github.com>
2019-10-17 23:46:43 -0700

    engops-46 POC to s3

--- trees/functions/tree.js
+++ trees/functions/tree.js
@@ -1,10 +1,33 @@
-var md5 = require('md5');
+const md5 = require('md5');
+const AWS = require('aws-sdk'); 
+AWS.config.update({region: 'us-east-1'});
 
 exports.handler = async (event, context) => {
     let body = `{}`
+    s3 = new AWS.S3({apiVersion: '2006-03-01'});
     if (event.httpMethod === "POST") {
+        const id = md5(event.body)
         //save json
-        body = `{"id": "${md5(event.body)}"}`
+       
+        const path = `interviews/trees/${id}`
+
+        const uploadParams = {Bucket: 'hingehealth-engineering', Key: path, Body: event.body};
+
+        const params = {
+            ACL: "public-read", 
+            Body: event.body, 
+            Bucket: 'hingehealth-engineering', 
+            Key: path
+           };
+           s3.putObject(params, function(err, data) {
+             if (err) body = `{"err": "${err}"}`; // an error occurred
+             else     body = `{"id": "${id}"}`;   // successful response
+             return {
+                statusCode: 200,
+                body: body
+              };
+           });
+
     } else if (event.queryStringParameters && event.queryStringParameters.id){
         const id = event.queryStringParameters.id
         //fetch json for id
